d0a8dea46eec738700b0d01281f403e8cde23c0f
Alberto Martínez Cánovas <amc051@inlumine.ual.es>
2025-02-24 09:24:06 +0100

    Clase vehículos terminada

---
+++ src/main/java/org/iesalandalus/programacion/tallermecanico/modelo/negocio/Vehiculos.java
@@ -0,0 +1,48 @@
+package org.iesalandalus.programacion.tallermecanico.modelo.negocio;
+
+import org.iesalandalus.programacion.tallermecanico.modelo.TallerMecanicoExcepcion;
+import org.iesalandalus.programacion.tallermecanico.modelo.dominio.Cliente;
+import org.iesalandalus.programacion.tallermecanico.modelo.dominio.Vehiculo;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class Vehiculos {
+    private final List<Vehiculo> coleccionVehiculos;
+
+    public Vehiculos() {
+        coleccionVehiculos = new ArrayList<>();
+    }
+
+    public List<Vehiculo> get() {
+        return new ArrayList<>(coleccionVehiculos);
+    }
+
+    public void insertar(Vehiculo vehiculo) throws TallerMecanicoExcepcion {
+        Objects.requireNonNull(vehiculo, "No se puede insertar un vehículo nulo.");
+        if (coleccionVehiculos.contains(vehiculo)) {
+            throw new TallerMecanicoExcepcion("Ya existe un vehículo con esa matrícula.");
+        }
+        coleccionVehiculos.add(vehiculo);
+    }
+
+    public Vehiculo buscar(Vehiculo vehiculo) {
+        Objects.requireNonNull(vehiculo, "No se puede buscar un vehículo nulo.");
+        Vehiculo buscado;
+        if (!coleccionVehiculos.contains(vehiculo)) {
+            buscado = null;
+        } else {
+            buscado = coleccionVehiculos.get(coleccionVehiculos.indexOf(vehiculo));
+        }
+        return buscado;
+    }
+
+    public void borrar(Vehiculo vehiculo) throws TallerMecanicoExcepcion {
+        Objects.requireNonNull(vehiculo, "No se puede borrar un vehículo nulo.");
+        if (!coleccionVehiculos.contains(vehiculo)) {
+            throw new TallerMecanicoExcepcion("No existe ningún vehículo con esa matrícula.");
+        }
+        coleccionVehiculos.remove(vehiculo);
+    }
+}
